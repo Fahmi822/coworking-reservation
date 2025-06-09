ee3bd7275db2fb6071183ad21f8a0578a59bbbc0
fahmihlel <fahmihlel822@gmail.com>
2025-06-10 01:19:05 +0200

    index.js

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import { resolvers } from './resolvers/resolvers.js';
+import { authMiddleware } from './utils/auth.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const typeDefs = readFileSync('./src/schema/schema.graphql', { encoding: 'utf-8' });
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: parseInt(process.env.PORT) || 4000 }  // Utilise le port 3001
+  listen: { port: parseInt(process.env.PORT) || 4000 },
+  context: authMiddleware
 });
 
 console.log(`🚀 Serveur prêt à ${url}`);
